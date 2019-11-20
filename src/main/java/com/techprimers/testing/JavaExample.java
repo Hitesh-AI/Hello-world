74a58acc4b28eee046f3d23356f0ceb659139e05
hitesh.pipaliya <hitesh.pipaliya@ajmerainfotech.com>
2019-11-20 17:52:28 +0530

    commit the

---
+++ src/main/java/com/techprimers/testing/JavaExample.java
@@ -0,0 +1,26 @@
+package com.techprimers.testing;
+
+import java.util.*; 
+
+public class JavaExample {
+
+	public static void main(String args[]){  
+	      ArrayList<String> alist=new ArrayList<String>();  
+	      alist.add("Steve");
+	      alist.add("Tim");
+	      alist.add("Lucy");
+	      alist.add("Pat");
+	      alist.add("Angela");
+	      alist.add("Tom");
+	  
+	      //displaying elements
+	      System.out.println(alist);
+	  
+	      //Adding "Steve" at the fourth position
+	      alist.add(3, "Steve");
+	  
+	      //displaying elements
+	      System.out.println(alist);
+	   }  
+
+}
